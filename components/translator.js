aeaac03089463173bc90d430d92e4df602d2d009
Clark Reyes <clarkenreyes@gmail.com>
2023-08-13 20:26:19 +0800

    feat: created tuples for american to british and vice versa

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,24 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
+
+const americanToBritish = [
+  ...Object.entries(americanOnly),
+  ...Object.entries(americanToBritishSpelling),
+  ...Object.entries(americanToBritishTitles),
+];
+
+const britishToAmerican = [
+  ...Object.entries(americanToBritishSpelling).map((elem) => elem.reverse()),
+  ...Object.entries(americanToBritishTitles).map((elem) => elem.reverse()),
+  ...Object.entries(britishOnly),
+];
 
 class Translator {
-
+  translate(text, locale, settings = { highlight: false }) {
+    const highlight = settings.highlight;
+  }
 }
 
 module.exports = Translator;
